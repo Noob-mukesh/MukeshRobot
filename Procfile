226d0bfbf7c31f019964442034d799b77119c234
Noob-mukesh <108251605+Noob-mukesh@users.noreply.github.com>
2022-07-21 12:29:59 +0530

    Add files via upload

---
+++ Procfile
@@ -0,0 +1,2 @@
+worker: python3 -m MukeshRobot
+ps:scale worker=1
---
+++ start.bat
@@ -0,0 +1,4 @@
+@echo off
+TITLE Mukesh Robot
+:: Enables virtual env mode and then starts Fallen
+env\scripts\activate.bat && py -m MukeshRobot
