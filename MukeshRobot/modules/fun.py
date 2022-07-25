e632172c53c0d359bb9958926662157a99e32e08
Noob-mukesh <108251605+Noob-mukesh@users.noreply.github.com>
2022-07-25 22:19:03 +0530

    Update fun.py

--- MukeshRobot/modules/fun.py
+++ MukeshRobot/modules/fun.py
@@ -2,11 +2,11 @@ import html
 import random
 import time
 
-import FallenRobot.modules.fun_strings as fun_strings
-from FallenRobot import dispatcher
-from FallenRobot.modules.disable import DisableAbleCommandHandler
-from FallenRobot.modules.helper_funcs.chat_status import is_user_admin
-from FallenRobot.modules.helper_funcs.extraction import extract_user
+import MukeshRobot.modules.fun_strings as fun_strings
+from MukeshRobot import dispatcher
+from MukeshRobot.modules.disable import DisableAbleCommandHandler
+from MukeshRobot.modules.helper_funcs.chat_status import is_user_admin
+from MukeshRobot.modules.helper_funcs.extraction import extract_user
 from telegram import ChatPermissions, ParseMode, Update
 from telegram.error import BadRequest
 from telegram.ext import CallbackContext, run_async
