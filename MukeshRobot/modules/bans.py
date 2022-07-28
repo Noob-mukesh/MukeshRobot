1d19ab8f426f9a3f9f5eface0b690a07142ada67
DarksideAbhi <109504373+DarksideAbhi@users.noreply.github.com>
2022-07-28 08:26:58 +0530

    Update bans.py

--- MukeshRobot/modules/bans.py
+++ MukeshRobot/modules/bans.py
@@ -425,7 +425,7 @@ dispatcher.add_handler(UNBAN_HANDLER)
 dispatcher.add_handler(ROAR_HANDLER)
 dispatcher.add_handler(KICKME_HANDLER)
 
-__mod_name__ = "​ʙᴀɴꜱ😵"
+__mod_name__ = "ʙᴀɴꜱ 🤧"
 __handlers__ = [
     BAN_HANDLER,
     TEMPBAN_HANDLER,
