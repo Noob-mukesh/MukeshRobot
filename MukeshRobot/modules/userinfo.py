1725cf27387f3bb7faba97f5ba52d146e4b9e5a8
DarksideAbhi <109504373+DarksideAbhi@users.noreply.github.com>
2022-07-29 21:02:29 +0530

    Update userinfo.py

--- MukeshRobot/modules/userinfo.py
+++ MukeshRobot/modules/userinfo.py
@@ -548,7 +548,7 @@ dispatcher.add_handler(GET_BIO_HANDLER)
 dispatcher.add_handler(SET_ABOUT_HANDLER)
 dispatcher.add_handler(GET_ABOUT_HANDLER)
 
-__mod_name__ = "Iɴꜰᴏs"
+__mod_name__ = "Iɴꜰᴏs 😧"
 __command_list__ = ["setbio", "bio", "setme", "me", "info"]
 __handlers__ = [
     ID_HANDLER,
