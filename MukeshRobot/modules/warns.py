985e69755a2f8d97b28f7019ce034d3b77663a58
DarksideAbhi <109504373+DarksideAbhi@users.noreply.github.com>
2022-07-29 20:57:15 +0530

    Update warns.py

--- MukeshRobot/modules/warns.py
+++ MukeshRobot/modules/warns.py
@@ -522,7 +522,7 @@ be a sentence, encompass it with quotes, as such: `/addwarn "very angry" This is
  ❍ /strongwarn <on/yes/off/no>*:* If set to on, exceeding the warn limit will result in a ban. Else, will just punch.
 """
 
-__mod_name__ = "Wᴀʀɴs"
+__mod_name__ = "Wᴀʀɴs 😔"
 
 WARN_HANDLER = CommandHandler(["warn", "dwarn"], warn_user, filters=Filters.group)
 RESET_WARN_HANDLER = CommandHandler(
