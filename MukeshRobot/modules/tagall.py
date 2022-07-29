3591e44e0033fefae1be54776ccb3c48fd4d9130
DarksideAbhi <109504373+DarksideAbhi@users.noreply.github.com>
2022-07-29 21:09:34 +0530

    Update tagall.py

--- MukeshRobot/modules/tagall.py
+++ MukeshRobot/modules/tagall.py
@@ -99,7 +99,7 @@ async def cancel_spam(event):
         return await event.respond("__Stopped Mention.__")
 
 
-__mod_name__ = "Tᴀɢ Aʟʟ​"
+__mod_name__ = "Tᴀɢ Aʟʟ 🚀​"
 __help__ = """
 ──「 Only for Admins 」──
 
