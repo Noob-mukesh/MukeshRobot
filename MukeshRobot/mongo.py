77dd3d76ad0569736c28b338838d9a6bb1fa1586
mukesh364 <90526750+mukesh364@users.noreply.github.com>
2022-08-04 03:39:26 +0530

    Create mongo.py

---
+++ MukeshRobot/mongo.py
@@ -0,0 +1,26 @@
+import asyncio
+import sys
+import logging as log
+
+from motor import motor_asyncio
+from pymongo import MongoClient
+from pymongo.errors import ServerSelectionTimeoutError
+
+from MukeshRobot import MONGO_DB_URI
+from MukeshRobot.confing import get_int_key, get_str_key
+
+
+MONGO_PORT = get_int_key("27017")
+MONGO_DB_URI = get_str_key("MONGO_DB_URI")
+MONGO_DB = "MukeshRobot"
+
+
+client = MongoClient()
+client = MongoClient(MONGO_DB_URI, MONGO_PORT)[MONGO_DB]
+motor = motor_asyncio.AsyncIOMotorClient(MONGO_DB_URI, MONGO_PORT)
+db = motor[MONGO_DB]
+db = client["mukeshrobot"]
+try:
+    asyncio.get_event_loop().run_until_complete(motor.server_info())
+except ServerSelectionTimeoutError:
+    sys.exit(log.critical("Can't connect to mongodb! Exiting..."))
