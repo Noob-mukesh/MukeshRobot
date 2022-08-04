f1cc6ba5b6344ae3c92c04e0ee6893df335d98d4
Noob-mukesh <108251605+Noob-mukesh@users.noreply.github.com>
2022-08-04 22:34:58 +0530

    Update country.py

--- MukeshRobot/modules/country.py
+++ MukeshRobot/modules/country.py
@@ -101,7 +101,7 @@ Time Zones:- {tom}
 Top Level Domain:- {lanester}
 wikipedia:- {wiki}</b>
 
-Gathered By [FALLEN](t.me/anonymous_was_bot)</b>
+Gathered By [ᴍᴜᴋᴇsʜ](t.me/itz_mst_boi)</b>
 """
 
     await borg.send_message(
