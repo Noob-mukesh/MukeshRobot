e848d002a42bb2d141b62d0f3ae0e2f022819a63
Noob-mukesh <108251605+Noob-mukesh@users.noreply.github.com>
2022-08-04 23:01:37 +0530

    Update blacklist_stickers.py

--- MukeshRobot/modules/blacklist_stickers.py
+++ MukeshRobot/modules/blacklist_stickers.py
@@ -507,7 +507,7 @@ Note:
  ❍ <sticker link> can be `https://t.me/addstickers/<sticker>` or just `<sticker>` or reply to the sticker message
 """
 
-__mod_name__ = "Bʟ-Sᴛɪᴄᴋ"
+__mod_name__ = "Bʟ-Sᴛɪᴄᴋ☠︎︎ "
 
 BLACKLIST_STICKER_HANDLER = DisableAbleCommandHandler(
     "blsticker", blackliststicker, admin_ok=True
