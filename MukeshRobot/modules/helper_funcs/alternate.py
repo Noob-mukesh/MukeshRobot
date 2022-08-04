b0d4dd2eaf4c4baad31ed6f575db8dc608bd22f1
mukesh364 <90526750+mukesh364@users.noreply.github.com>
2022-08-04 14:16:10 +0530

    Update alternate.py

--- MukeshRobot/modules/helper_funcs/alternate.py
+++ MukeshRobot/modules/helper_funcs/alternate.py
@@ -22,3 +22,18 @@ def typing_action(func):
         return func(update, context, *args, **kwargs)
 
     return command_func
+
+def send_action(action):
+    """Sends `action` while processing func command."""
+
+    def decorator(func):
+        @wraps(func)
+        def command_func(update, context, *args, **kwargs):
+            context.bot.send_chat_action(
+                chat_id=update.effective_chat.id, action=action
+            )
+            return func(update, context, *args, **kwargs)
+
+        return command_func
+
+    return decorator
