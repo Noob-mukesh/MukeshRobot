c4709c378265055b4ab2b341104cfc8f1759f22d
mukesh364 <90526750+mukesh364@users.noreply.github.com>
2022-08-04 12:56:51 +0530

    Update kumuii.py

--- MukeshRobot/modules/kumuii.py
+++ MukeshRobot/modules/kumuii.py
@@ -22,7 +22,7 @@ async def _(fry):
     if reply_message.sender.bot:
         await fry.edit("`Reply to a asticker to destroy`")
         return
-    chat = "@mukeshbotzone"
+    chat = "@image_deepfrybot"
     message_id_to_reply = fry.message.reply_to_msg_id
     async with pbot.conversation(chat) as conv:
         try:
