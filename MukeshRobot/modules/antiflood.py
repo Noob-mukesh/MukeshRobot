0353544d96668ec8f78a11419a4cbc4b41092c53
DarksideAbhi <109504373+DarksideAbhi@users.noreply.github.com>
2022-08-04 09:54:13 +0530

    Update antiflood.py

--- MukeshRobot/modules/antiflood.py
+++ MukeshRobot/modules/antiflood.py
@@ -419,7 +419,7 @@ will result in restricting that user.
  `1w` = 1 week
  """
 
-__mod_name__ = "ᴀɴᴛɪ-ꜰʟᴏᴏᴅ❗"
+__mod_name__ = "+ᴀɴᴛɪ-ꜰʟᴏᴏᴅ❗"
 
 FLOOD_BAN_HANDLER = MessageHandler(
     Filters.all & ~Filters.status_update & Filters.group, check_flood
