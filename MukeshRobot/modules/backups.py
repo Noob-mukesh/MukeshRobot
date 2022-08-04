ec2013086a6fa9deeb881fb6830086cc4880bf8e
Noob-mukesh <108251605+Noob-mukesh@users.noreply.github.com>
2022-08-04 23:26:16 +0530

    Update backups.py

--- MukeshRobot/modules/backups.py
+++ MukeshRobot/modules/backups.py
@@ -367,7 +367,7 @@ def get_chat(chat_id, chat_data):
         return {"status": False, "value": False}
 
 
-__mod_name__ = "Bᴀᴄᴋᴜᴘ"
+__mod_name__ = "Bᴀᴄᴋᴜᴘ♲︎︎︎"
 
 __help__ = """
 *Only for group owner:*
