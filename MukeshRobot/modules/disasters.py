138548af337431367a44231390a2d883b0e79798
Noob-mukesh <108251605+Noob-mukesh@users.noreply.github.com>
2022-08-05 11:09:28 +0530

    Update disasters.py

--- MukeshRobot/modules/disasters.py
+++ MukeshRobot/modules/disasters.py
@@ -645,7 +645,7 @@ Group admins/group owners do not need these commands.
  ❍ /logs Get heroku dyno logs.
 
 `⚠️ Read from top`
-Visit @{SUPPORT_CHAT} for more information.
+Visit @worldwide\_Friend\_zone for more information.
 """
 
 SUDO_HANDLER = CommandHandler(("addsudo", "adddragon"), addsudo)
@@ -678,7 +678,7 @@ dispatcher.add_handler(SUPPORTLIST_HANDLER)
 dispatcher.add_handler(SUDOLIST_HANDLER)
 dispatcher.add_handler(DEVLIST_HANDLER)
 
-__mod_name__ = "Devs​"
+__mod_name__ = "ᴅᴇᴠs🔺​"
 __handlers__ = [
     SUDO_HANDLER,
     SUPPORT_HANDLER,
