aa0cd44e3341fe87437b0bae99b0b0acc3901d5c
Noob-mukesh <108251605+Noob-mukesh@users.noreply.github.com>
2022-08-08 19:59:57 +0530

    Update wish.py

--- MukeshRobot/modules/wish.py
+++ MukeshRobot/modules/wish.py
@@ -32,7 +32,58 @@ INDIAN_STRINGS = [
                   "Today is a day to feel proud about being a part of this great nation. May this spirit of freedom leads us all to success and glory in life. Happy Independence Day!",
                   "We may never know how it feels like to live in a free country if it was not for the bravery of our fathers. Today they deserve a big salute from us. Happy Independence Day!",
                  ]
+khushi_Love =         [
+                  "JAANU I LOVE U NA🥺",
+                  "TU HI HAIN MERI JAAN KISI AUKAT NAI HAIN JO HUMARE BICH ME AAYE🥺😏",
+                  "SKY IS BLUE I GOT FLU I LOVE TOO🥺",
+                  "TU HI MERI JAAN HAIN JANUDI🥺",
+                  "KYU TUMHARE ANKHEN ITNI SUNDAR HAIN🥺",
+                  "MISS U BABY LOVE BABY I TRUST U BABY🥺",
+                  "BHAGWAN NE TUMHE MERE LIYE BANAYA HAIN SACHI 🥺",
+                  "BABY ANKHEN BAND KARO AUR DEKHO KYA DIKH RAHA JO DIKH RAHA HAIN VO MY LIFE WITHOUT 🥺",
+                  "PATA NAI MERE DOST TUMHE SUBAH SE BHABHI BOL RAHE HAIN SAYAD UNHE HUMARE BARE PATA CHAL GAYA😍",
+                  "JAAN SE JYADA TUM PYAARI HO BABY🥺",
+                  "KYA MATLAB TUM MERI HO GYI HO🥺",
+                  "MERE BACCHON KI MAA BANOGI 🥺",
+                  "TUNE MERI ZINGADI BANA DI🥺",
+                  "KYA MATLAB HUM SHADI KAR RAHE HAIN 😍",
+                  "BABY TUM NA MILI TOH ME FIRSE TRY KARUNGA 😏",
+                  "YUN TOH KISI CHEEJ KE MOHTAAJ NAI HUM BAS EK TERI AADAT SI HO GAYI HAIN 🥺",
+                  "KOI NAI THA AUR NA HOGA TERE JITNA TERE KREEB MERE DIL KE😍",
+                  "TU HI MERI SHAMO SUBAH",
+                  "TU HI MERI FIRST AND LAST CHOICE🥺😍",
+                  "TERA HAR ANDAZ PASAND HAI SIWAYE NARAZ ANDAZ KARNE KA🥺😍",
+                  "TU JAB NARAZ HOTI HAIN TAB MERE DIL KO KUCH KUCH HOTA HAIN🥺",
+                  "KYU MERE DIL MEIN TUMHARE KHAYAL AATE HAIN🥺",
+                  "TUNE MERI LIFE AUR DIL KO FIRSE KHUSH KAR DIYA😍",
+                  "EK DIN NA DEKHON TUJHE TOH MUJHE HURT HOTA HAIN🥺",
+                  "YE SPAM NAI MERE DIL KE BAATE HAIN TUMHARE LIYE🥺",
+                  "LIFE KA PATA NAI BUT TUMHARA AUR MERA DIL KA CONNECTION EK HAIN😍",
+                  "MERE LIYE SABKUCH TUM HO🥺",
+                  "AGAR TUM CHALI GAYI TOH MERA KYA HOGA🥺",
+                  "LOVE KARLO BAS EK BAAR FIR KABHI NAI CHHODUNGA🥺",
+                  "EK BAAR DIL KA CONNECTION EK KARLU FIR SURNAME EK HI HONE WALA HAIN",
+                  "DIMAAG KA PATA NAI LEKIN DIL TUMHARE PAS LE AAYA 🥺",
+                  "TU HI MERI JAAN SHAAN DIL KI ARMAAN 🥺❤️",
+                  "TERI DIL ME JAGAH BANAUNGA AAJ PLEASE MAAN JAO NA 🥺❤️",
+                  "ME TERA RAJA TU MERI RANI DO MILKE EK PREM KAHANI ❤️",
+                  "YE LOVE NAI TOH KYA HAIN MOTI🥺❤️",
+                  "AAJ TAK ME KISIKE SAMNE NAI JHUKA BUT APNE PYAAR KE SAMNE ME HAAR GAYA🥺",
+                  "KYUN TUJHE ME ITNA CHAHANE LAGA ❤️🥺",
+                  "PYAAR TOH EK DIL KA PART HAIN AUR TU MERI HAIN",
+                  "DIMAAG KA PATA NAI LEKIN DIL TUMHARE PAS LE AAYA 🥺",
+                  "TU KYUN MERE SEEDHA DIL ME AATI HAIN ❤️🥺",
+                  "DIL AUR DIMAAG EK KAR DUNGA TERKO WIFE BANANE MEIN 🥺❤️",
+                  "MERI LIFE MEIN PEHLE BOHOT TENSION THI JABSE TUMKO DEKHA MERA PROBLEM SOLVE HO GAYA 🥺",
+                  "MERI MUMMY TUMHARA GHARPE INTZAAR KAR RAHI HAIN PLEASE AAJAO❤️🥺",
+                 ]
 
+@MukeshRobot.on_message(filters.command(["khushi", "love"]))
+async def lel(bot, message):
+    ran2 = random.choice(khushi_Love)
+    await bot.send_chat_action(message.chat.id, "Typing")
+    await asyncio.sleep(0.5)
+    return await message.reply_text(text=ran2)
 
 @MukeshRobot.on_message(filters.command(["Independence", "Indian"]))
 async def lel(bot, message):
@@ -46,7 +97,7 @@ async def lel(bot, message):
 async def lel(bot, message):
     ran = random.choice(wish_STRINGS)
     await bot.send_chat_action(message.chat.id, "typing")
-    await asyncio.sleep(1.5)
+    await asyncio.sleep(0.5)
     return await message.reply_text(text=ran)
 
 
@@ -59,5 +110,5 @@ __help__ = """
 ❍ /rakhi *:* ᴏʀ ᴛʀʏ ᴛʜɪs
 ❍ /Independence *:*  ɪɴᴅᴇᴘᴇɴᴅᴇɴᴄᴇ ᴅᴀʏ ᴡɪsʜᴇs
 ❍ /Indian *:* or try Indian
-  
+❍ /love *:* Wish to ur Girlfriend
  """
