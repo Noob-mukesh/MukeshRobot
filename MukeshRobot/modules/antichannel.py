4274b69682497402d256fcfe05d2f3bff838a700
Mukesh <108251605+Noob-mukesh@users.noreply.github.com>
2022-08-11 15:24:52 +0530

    Update antichannel.py

--- MukeshRobot/modules/antichannel.py
+++ MukeshRobot/modules/antichannel.py
@@ -13,16 +13,16 @@ async def channel_toggle(db, message: Message):
     if status == "on":
         if chat_id not in db:
             db.append(chat_id)
-            text = "**Anti Channel Mode `enabled` ✅. I will delete all message that send with channel names. Dare to leap**"
+            text = "**ᴀɴᴛɪ ᴄʜᴀɴɴᴇʟ ᴍᴏᴅᴇ ɪs 'ᴇɴᴀʙʟᴇᴅ'.ɪ ᴡɪʟʟ ᴅᴇʟᴇᴛᴇ ᴀʟʟ ᴍᴇssᴀɢᴇ ᴛʜᴀᴛ sᴇɴᴅ ᴡɪᴛʜ ᴄʜᴀɴɴᴇʟ ɴᴀᴍᴇs🛡️**"
             return await eor(message, text=text)
-        await eor(message, text="antichannel Is Already Enabled.")
+        await eor(message, text="ᴀɴᴛɪᴄʜᴀɴɴᴇʟ ᴍᴏᴅᴇ ɪs ᴀʟʀᴇᴀᴅʏ ᴇɴᴀʙʟᴇᴅ.")
     elif status == "off":
         if chat_id in db:
             db.remove(chat_id)
-            return await eor(message, text="antichannel Disabled!")
-        await eor(message, text=f"**Anti Channel Mode Successfully Deactivated In The Chat** {message.chat.id} ❌")
+            return await eor(message, text="ᴀɴᴛɪᴄʜᴀɴɴᴇʟ ᴅɪsᴀʙʟᴇᴅ ʙᴀʙʏ🔻")
+        await eor(message, text=f"**ᴀɴᴛɪ ᴄʜᴀɴɴᴇʟ ᴍᴏᴅᴇ sᴜᴄᴇssғᴜʟʟʏ ᴅᴇᴀᴄᴛɪᴠᴀᴛᴇᴅ ɪɴ ᴛʜɪs ᴄʜᴀᴛ** {message.chat.id} ❌")
     else:
-        await eor(message, text="I undestand `/antichannel on` and `/antichannel off` only")
+        await eor(message, text="ɪ ᴜɴᴅᴇʀsᴛᴀɴᴅ  `/antichannel on` and `/antichannel off` ᴏɴʟʏ")
 
 
 # Enabled | Disable antichannel
@@ -32,7 +32,7 @@ async def channel_toggle(db, message: Message):
 @capture_err
 async def antichannel_status(_, message: Message):
     if len(message.command) != 2:
-        return await eor(message, text="I undestand `/antichannel on` and `/antichannel off` only")
+        return await eor(message, text="ɪ ᴜɴᴅᴇʀsᴛᴀɴᴅ `/antichannel on` and `/antichannel off` ᴏɴʟʏ")
     await channel_toggle(active_channel, message)
 
 
@@ -75,6 +75,8 @@ your groups to stop anonymous channels sending messages into your chats.
         - text
         
 **Admin Commands:**
+ ♦️[ᴍᴀᴅᴇ ʙʏ](t.me/itz_mst_boi)
  - /antichannel [on / off] - Anti- channel  function 
-**Note** : If linked channel  send any containing characters in this type when on  function no del    
+**Note** : If linked channel  send any containing characters in this type when on  function no del   
+ 🚩ᴘᴏᴡᴇʀᴇᴅ ʙʏ : [ᴍᴜᴋᴇsʜ ʙᴏᴛ ᴢᴏɴᴇ](t.me/mukeshbotzone)
  """
