0f29f8da98b06de46f8bc2880b35897d3d6bc6c8
ɴᴏᴏʙ - ᴍᴜᴋᴇsʜ <108251605+Noob-mukesh@users.noreply.github.com>
2022-08-14 07:16:16 +0530

    Create insult.py

---
+++ MukeshRobot/modules/insult.py
@@ -0,0 +1,87 @@
+#Piro people dont remove this line 
+# github - Noob-Mukesh
+import random
+from telegram.ext import run_async, Filters
+from telegram import Message, Chat, Update, Bot, MessageEntity
+from MukeshRobot import dispatcher
+from MukeshRobot.modules.disable import DisableAbleCommandHandler
+
+SFW_STRINGS = (
+    "Owww ... Such a stupid idiot.",
+    "Don't drink and type.",
+    "I think you should go home or better a mental asylum.",
+    "Command not found. Just like your brain.",
+    "Do you realize you are making a fool of yourself? Apparently not.",
+    "You can type better than that.",
+    "Bot rule 544 section 9 prevents me from replying to stupid humans like you.",
+    "Sorry, we do not sell brains.",
+    "Believe me you are not normal.",
+    "I bet your brain feels as good as new, seeing that you never use it.",
+    "If I wanted to kill myself I'd climb your ego and jump to your IQ.",
+    "Zombies eat brains... you're safe.",
+    "You didn't evolve from apes, they evolved from you.",
+    "Come back and talk to me when your I.Q. exceeds your age.",
+    "I'm not saying you're stupid, I'm just saying you've got bad luck when it comes to thinking.",
+    "What language are you speaking? Cause it sounds like bullshit.",
+    "Stupidity is not a crime so you are free to go.",
+    "You are proof that evolution CAN go in reverse.",
+    "I would ask you how old you are but I know you can't count that high.",
+    "As an outsider, what do you think of the human race?",
+    "Brains aren't everything. In your case they're nothing.",
+    "Ordinarily people live and learn. You just live.",
+    "I don't know what makes you so stupid, but it really works.",
+    "Keep talking, someday you'll say something intelligent! (I doubt it though)",
+    "Shock me, say something intelligent.",
+    "Your IQ's lower than your shoe size.",
+    "Alas! Your neurotransmitters are no more working.",
+    "Are you crazy you fool.",
+    "Everyone has the right to be stupid but you are abusing the privilege.",
+    "I'm sorry I hurt your feelings when I called you stupid. I thought you already knew that.",
+    "You should try tasting cyanide.",
+    "Your enzymes are meant to digest rat poison.",
+    "You should try sleeping forever.",
+    "Pick up a gun and shoot yourself.",
+    "You could make a world record by jumping from a plane without parachute.",
+    "Stop talking BS and jump in front of a running bullet train.",
+    "Try bathing with Hydrochloric Acid instead of water.",
+    "Try this: if you hold your breath underwater for an hour, you can then hold it forever.",
+    "Go Green! Stop inhaling Oxygen.",
+    "God was searching for you. You should leave to meet him.",
+    "give your 100%. Now, go donate blood.",
+    "Try jumping from a hundred story building but you can do it only once.",
+    "You should donate your brain seeing that you never used it.",
+    "Volunteer for target in an firing range.",
+    "Head shots are fun. Get yourself one.",
+    "You should try swimming with great white sharks.",
+    "You should paint yourself red and run in a bull marathon.",
+    "You can stay underwater for the rest of your life without coming back up.",
+    "How about you stop breathing for like 1 day? That'll be great.",
+    "Try provoking a tiger while you both are in a cage.",
+    "Have you tried shooting yourself as high as 100m using a canon.",
+    "You should try holding TNT in your mouth and igniting it.",
+    "Try playing catch and throw with RDX its fun.",
+    "I heard phogine is poisonous but i guess you wont mind inhaling it for fun.",
+    "Launch yourself into outer space while forgetting oxygen on Earth.",
+    "You should try playing snake and ladders, with real snakes and no ladders.",
+    "Dance naked on a couple of HT wires.",
+    "Active Volcano is the best swimming pool for you.",
+    "You should try hot bath in a volcano.",
+    "Try to spend one day in a coffin and it will be yours forever.",
+    "Hit Uranium with a slow moving neutron in your presence. It will be a worthwhile experience.",
+    "You can be the first person to step on sun. Have a try.",
+)
+
+def insult(update, context):
+    context.bot.sendChatAction(update.effective_chat.id, "typing") # Bot typing before send messages
+    message = update.effective_message
+    if message.reply_to_message:
+      message.reply_to_message.reply_text(random.choice(SFW_STRINGS))
+    else:
+      message.reply_text(random.choice(SFW_STRINGS))
+
+INSULT_HANDLER = DisableAbleCommandHandler("insult", insult)
+
+dispatcher.add_handler(INSULT_HANDLER)
+__mod_name__= "Insult"
+__help__= """
+ * /insult - to insult  ur friend """
