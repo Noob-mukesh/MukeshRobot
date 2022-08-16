e0c10b6d0af8416a5d0578d062edb9d215ffdbba
ɴᴏᴏʙ - ᴍᴜᴋᴇsʜ <108251605+Noob-mukesh@users.noreply.github.com>
2022-08-16 20:46:34 +0530

    Update functions.py

--- MukeshRobot/utils/functions.py
+++ MukeshRobot/utils/functions.py
@@ -9,9 +9,3 @@ async def make_carbon(code):
         image = BytesIO(await resp.read())
     image.name = "carbon.png"
     return image
-def get_urls_from_text(text: str) -> bool:
-    regex = r"""(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]
-                [.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(
-                \([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\
-                ()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))""".strip()
-    return [x[0] for x in findall(regex, str(text))]
