73a388fb379924fad50d80b2059d8203f666f5e2
ɴᴏᴏʙ - ᴍᴜᴋᴇsʜ <108251605+Noob-mukesh@users.noreply.github.com>
2022-08-26 00:33:56 +0530

    Update nightmode.py

--- MukeshRobot/modules/nightmode.py
+++ MukeshRobot/modules/nightmode.py
@@ -160,4 +160,4 @@ __help__ = """
 *Note:* Night Mode chats get Automatically closed at 12 am(IST) and Automatically openned at 6 am(IST) to Prevent Night Spams.
 """
 
-__mod_name__ = "Nɪɢʜᴛ​"
+__mod_name__ = "Nɪɢʜᴛ​⛄"
