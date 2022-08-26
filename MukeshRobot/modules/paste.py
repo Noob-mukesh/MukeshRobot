9c4ca792d217527401b86d3f02095c7bd299e68b
ɴᴏᴏʙ - ᴍᴜᴋᴇsʜ <108251605+Noob-mukesh@users.noreply.github.com>
2022-08-26 06:04:26 +0530

    Update paste.py

--- MukeshRobot/modules/paste.py
+++ MukeshRobot/modules/paste.py
@@ -62,7 +62,7 @@ async def paste_func(_, message):
     return await m.edit(link)
 
 
-__mod_name__ = "Pᴀsᴛᴇ​"
+__mod_name__ = "Pᴀs​ᴛᴇ💈"
 __help__ = """
  Pastes the given file and shows you the result
 
