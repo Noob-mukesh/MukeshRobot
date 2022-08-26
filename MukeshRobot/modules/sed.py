74edc97f23ea6d7f0b1f18788941a0618c87d624
ɴᴏᴏʙ - ᴍᴜᴋᴇsʜ <108251605+Noob-mukesh@users.noreply.github.com>
2022-08-26 06:18:10 +0530

    Update sed.py

--- MukeshRobot/modules/sed.py
+++ MukeshRobot/modules/sed.py
@@ -123,7 +123,7 @@ def sed(update: Update, context: CallbackContext):
             update.effective_message.reply_to_message.reply_text(text)
 
 
-__mod_name__ = "Sed/Regex"
+__mod_name__ = "sᴇᴅ💡"
 
 SED_HANDLER = DisableAbleMessageHandler(
     Filters.regex(r"s([{}]).*?\1.*".format("".join(DELIMITERS))), sed, friendly="sed"
