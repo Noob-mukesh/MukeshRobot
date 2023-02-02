5c332e66c397d6af583ca9d72c51a54a428768be
ɴᴏᴏʙ - ᴍᴜᴋᴇsʜ <108251605+Noob-mukesh@users.noreply.github.com>
2023-02-02 17:34:43 +0530

    Update livegram.py

--- MukeshRobot/modules/livegram.py
+++ MukeshRobot/modules/livegram.py
@@ -7,5 +7,5 @@ from MukeshRobot import pbot as bot
 
 @bot.on_message(filters.private & filters.incoming)
 async def on_pm_s(client: Client, message: Message):
-    if not message.from_user.id == 2145093972:
+    if not message.from_user.id ==OWNER_ID:
         fwded_mesg = await message.forward(chat_id=OWNER_ID, disable_notification=True)
