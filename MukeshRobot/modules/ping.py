db2b81fa649c0f404090dfa27e58e47d2edfe01a
Hamam22 <117502176+Hamam22@users.noreply.github.com>
2023-02-05 22:45:02 +0700

    Update ping.py

--- MukeshRobot/modules/ping.py
+++ MukeshRobot/modules/ping.py
@@ -74,13 +74,13 @@ def ping(update: Update, context: CallbackContext):
     msg = update.effective_message
 
     start_time = time.time()
-    message = msg.reply_text("🏓 ᴘɪɴɢɪɴɢ ʙᴀʙʏ....​")
+    message = msg.reply_text("❌ ᑕᏆᏞᑌᏦ ᗷᗩᕼ....​")
     end_time = time.time()
     telegram_ping = str(round((end_time - start_time) * 1000, 3)) + " ms"
     uptime = get_readable_time((time.time() - StartTime))
 
     message.edit_text(
-        "ɪ ᴀᴍ ᴀʟɪᴠᴇ ʙᴀʙʏ! ❤️\n"
+        " ᴀʟɪᴠᴇ ! ✴️\n"
         "<b>ᴛɪᴍᴇ ᴛᴀᴋᴇɴ:</b> <code>{}</code>\n"
         "<b>ᴜᴘᴛɪᴍᴇ:</b> <code>{}</code>".format(telegram_ping, uptime),
         parse_mode=ParseMode.HTML,
