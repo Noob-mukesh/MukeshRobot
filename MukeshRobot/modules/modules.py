0e2effa6c24e6bf11804c82b3b563e6aa5e8cdba
ɴᴏᴏʙ - ᴍᴜᴋᴇsʜ <108251605+Noob-mukesh@users.noreply.github.com>
2023-03-03 23:44:09 +0530

    Update modules.py

--- MukeshRobot/modules/modules.py
+++ MukeshRobot/modules/modules.py
@@ -184,4 +184,4 @@ dispatcher.add_handler(LOAD_HANDLER)
 dispatcher.add_handler(UNLOAD_HANDLER)
 dispatcher.add_handler(LISTMODULES_HANDLER)
 
-__mod_name__ = "⍟ ᴍᴏᴅᴜʟᴇs ⍟"
+__mod_name__ = "ᴍᴏᴅᴜʟᴇs"
