7c8308963d28651dfa516f4dbfe93f7d9598cd1a
ɴᴏᴏʙ - ᴍᴜᴋᴇsʜ <108251605+Noob-mukesh@users.noreply.github.com>
2023-03-03 10:42:25 +0530

    Update google.py

--- MukeshRobot/modules/google.py
+++ MukeshRobot/modules/google.py
@@ -266,7 +266,7 @@ async def apk(e):
         await e.reply("Exception Occured:- " + str(err))
 
 
-__mod_name__ = "Gᴏᴏɢʟᴇ🎉"
+__mod_name__ = "⍟ Gᴏᴏɢʟᴇ ⍟"
 
 __help__ = """
  ❍ /google <text>*:* Perform a google search
