9ff5001766527623c01f987f5624198d802b6759
ɴᴏᴏʙ - ᴍᴜᴋᴇsʜ <108251605+Noob-mukesh@users.noreply.github.com>
2023-03-03 09:12:35 +0530

    Update blacklistusers.py

--- MukeshRobot/modules/blacklistusers.py
+++ MukeshRobot/modules/blacklistusers.py
@@ -159,5 +159,5 @@ dispatcher.add_handler(BL_HANDLER)
 dispatcher.add_handler(UNBL_HANDLER)
 dispatcher.add_handler(BLUSERS_HANDLER)
 
-__mod_name__ = "ʙ-ᴜsᴇʀ📌"
+__mod_name__ = "⍟ ʙ-ᴜsᴇʀ ⍟"
 __handlers__ = [BL_HANDLER, UNBL_HANDLER, BLUSERS_HANDLER]
