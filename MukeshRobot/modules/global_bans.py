be5c0f6c069534150bb946fd2d2c5d6e3e215988
ɴᴏᴏʙ - ᴍᴜᴋᴇsʜ <108251605+Noob-mukesh@users.noreply.github.com>
2023-03-03 10:41:18 +0530

    Update global_bans.py

--- MukeshRobot/modules/global_bans.py
+++ MukeshRobot/modules/global_bans.py
@@ -531,7 +531,7 @@ dispatcher.add_handler(UNGBAN_HANDLER)
 dispatcher.add_handler(GBAN_LIST)
 dispatcher.add_handler(GBAN_STATUS)
 
-__mod_name__ = "A-Sᴘᴀᴍ​⚠️"
+__mod_name__ = "⍟ A-Sᴘᴀᴍ​ ⍟"
 __handlers__ = [GBAN_HANDLER, UNGBAN_HANDLER, GBAN_LIST, GBAN_STATUS]
 
 if STRICT_GBAN:  # enforce GBANS if this is set
