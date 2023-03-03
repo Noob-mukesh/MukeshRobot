c64f800b45dd3ebd564b63a82765ec9f2e12cd5c
ɴᴏᴏʙ - ᴍᴜᴋᴇsʜ <108251605+Noob-mukesh@users.noreply.github.com>
2023-03-03 21:46:47 +0530

    Update myinfo.py

--- MukeshRobot/modules/myinfo.py
+++ MukeshRobot/modules/myinfo.py
@@ -78,5 +78,6 @@ async def callback_query_handler(event):
 
 
 __command_list__ = ["myinfo"]
-__mod_name__ = "ɪɴғᴏ❖"
-__help__ = """ /myinfo to get your info """
+__mod_name__ = "⍟ ɪɴғᴏ ⍟"
+__help__ = """
+ /myinfo  ᴛᴏ ɢᴇᴛ ʏᴏᴜʀ ɪɴғᴏ """
