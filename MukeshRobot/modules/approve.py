f02d13b101b76472dfe50afc0aa6beb30074fb48
ɴᴏᴏʙ - ᴍᴜᴋᴇsʜ <108251605+Noob-mukesh@users.noreply.github.com>
2023-03-05 08:52:10 +0530

    Update approve.py

--- MukeshRobot/modules/approve.py
+++ MukeshRobot/modules/approve.py
@@ -225,6 +225,6 @@ dispatcher.add_handler(APPROVAL)
 dispatcher.add_handler(UNAPPROVEALL)
 dispatcher.add_handler(UNAPPROVEALL_BTN)
 
-__mod_name__ = "Aᴘᴘʀᴏᴠᴇ ⍟"
+__mod_name__ = "⍟ Aᴘᴘʀᴏᴠᴇ ⍟"
 __command_list__ = ["approve", "unapprove", "approved", "approval"]
 __handlers__ = [APPROVE, DISAPPROVE, APPROVED, APPROVAL]
