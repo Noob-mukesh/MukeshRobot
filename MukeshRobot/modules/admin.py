42b0573dd8253afe95f02480e760e8a563404d8f
ɴᴏᴏʙ - ᴍᴜᴋᴇsʜ <108251605+Noob-mukesh@users.noreply.github.com>
2023-03-05 08:36:00 +0530

    Update admin.py

--- MukeshRobot/modules/admin.py
+++ MukeshRobot/modules/admin.py
@@ -1037,7 +1037,7 @@ dispatcher.add_handler(DEMOTE_HANDLER)
 dispatcher.add_handler(SET_TITLE_HANDLER)
 dispatcher.add_handler(ADMIN_REFRESH_HANDLER)
 
-__mod_name__ = "⍟ Aᴅᴍɪɴs ⍟"
+__mod_name__ = "Aᴅᴍɪɴs ⍟"
 __command_list__ = [
     "setdesc" "setsticker" "setgpic" "delgpic" "setgtitle" "adminlist",
     "admins",
