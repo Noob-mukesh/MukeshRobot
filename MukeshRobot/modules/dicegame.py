4567a9caf725cbffd5bf8f339cde557b0b1ed0ff
ɴᴏᴏʙ - ᴍᴜᴋᴇsʜ <108251605+Noob-mukesh@users.noreply.github.com>
2023-04-07 13:13:29 +0530

    Create dicegame.py

---
+++ MukeshRobot/modules/dicegame.py
@@ -0,0 +1,46 @@
+from pyrogram import Client, enums, filters
+#from config import *
+import asyncio
+from MukeshRobot import pbot as mukesh
+
+from pyrogram.handlers import MessageHandler
+
+
+@mukesh.on_message(filters.command("dice"))
+async def dice(bot, message):
+    x=await bot.send_dice(message.chat.id)
+    m=x.dice.value
+    await message.reply_text(f"Hey {message.from_user.first_name} your Score is : {m}")
+  
+@mukesh.on_message(filters.command("dart"))
+async def dart(bot, message):
+    x=await bot.send_dice(message.chat.id, "🎯")
+    m=x.dice.value
+    await message.reply_text(f"Hey {message.from_user.first_name} your Score is : {m}")
+
+@mukesh.on_message(filters.command("basket"))
+async def basket(bot, message):
+    x=await bot.send_dice(message.chat.id, "🏀")
+    m=x.dice.value
+    await message.reply_text(f"Hey {message.from_user.first_name} your Score is : {m}")
+@mukesh.on_message(filters.command("jackpot"))
+async def basket(bot, message):
+    x=await bot.send_dice(message.chat.id, "🎰")
+    m=x.dice.value
+    await message.reply_text(f"Hey {message.from_user.first_name} your Score is : {m}")
+@mukesh.on_message(filters.command("ball"))
+async def basket(bot, message):
+    x=await bot.send_dice(message.chat.id, "🎳")
+    m=x.dice.value
+    await message.reply_text(f"Hey {message.from_user.first_name} your Score is : {m}")
+__help__ = """
+ Play Game With Emojis:
+/dice - Dice 🎲
+/dart - Dart 🎯
+/basket - Basket Ball 🏀
+/ball - Bowling Ball 🎳
+/football - Football ⚽
+/jackpot - Spin slot machine 🎰
+ """
+
+__mod_name__ = "⍟ ɢᴀᴍᴇꜱ ⍟"
