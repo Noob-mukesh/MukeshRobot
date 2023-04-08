f725815619b0d6b94e0b71f109b2d2b7a2cb50da
ɴᴏᴏʙ - ᴍᴜᴋᴇsʜ <108251605+Noob-mukesh@users.noreply.github.com>
2023-04-08 20:34:05 +0530

    Update encrypt.py

--- MukeshRobot/modules/encrypt.py
+++ MukeshRobot/modules/encrypt.py
@@ -1,14 +1,13 @@
 import secureme
 from MukeshRobot import pbot as mukesh
-@mukesh.on_message(filters.command("encyrpt"))
+
+@mukesh.on_message(filters.command("encrypt"))
 async def secure(bot, message):
-    if len(message.command) < 2:
-        return await message.reply_text("**Example:**\n\n`/encyrpt Mukesh `")
-    m = message.text.split(' ')[1]
-    try:
-        k = secureme.encrypt(m)
-        await message.reply_text(k)
+     if len(message.command) < 2:
+           return await message.reply_text("Example:\n\n/encrypt Mukesh ")
+      m = message.text.split(' ')[1]
+     try:
+           k = secureme.encrypt(m)
+           await message.reply_text(k)
     except Exception as e:
-    	await message.reply_text(f"Error {e}")      
-    	      
-    	
+        await message.reply_text(f"Error {e}")
