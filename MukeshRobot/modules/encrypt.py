a25bec2865358f30c85a4a6e33af6fd109499556
ɴᴏᴏʙ - ᴍᴜᴋᴇsʜ <108251605+Noob-mukesh@users.noreply.github.com>
2023-04-08 20:57:58 +0530

    Update encrypt.py

--- MukeshRobot/modules/encrypt.py
+++ MukeshRobot/modules/encrypt.py
@@ -7,7 +7,7 @@ from MukeshRobot import pbot as mukesh
 async def countryinfo(bot, message):
     if len(message.command) < 2:
         return await message.reply_text("**Example:**\n\n`/encyrpt India`")
-    m = message.text.split(' ')[1]
+    m = message.text.split(' ',1)[1]
     try:
         Secure = secureme.encrypt(m)
         
