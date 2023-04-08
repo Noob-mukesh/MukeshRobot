1d95064de31c00484df7665ce52ce670e46394c5
ɴᴏᴏʙ - ᴍᴜᴋᴇsʜ <108251605+Noob-mukesh@users.noreply.github.com>
2023-04-08 21:16:46 +0530

    Update encrypt.py

--- MukeshRobot/modules/encrypt.py
+++ MukeshRobot/modules/encrypt.py
@@ -11,22 +11,22 @@ async def encyrpt(bot, message):
     try:
         Secure = secureme.encrypt(m)
         
-        await message.reply_text(Secure)
+        await message.reply_text(f"`{Secure}`")
         
 
     except Exception as e:
-        await message.reply_text(e)
+        await message.reply_text(f"Error {e}")
 
 @mukesh.on_message(filters.command("decrypt"))
 async def decrypt(bot, message):
     if len(message.command) < 2:
-        return await message.reply_text("**Example:**\n\n`/encyrpt India`")
+        return await message.reply_text("**Example:**\n\n`/decrypt Nsinf`")
     m = message.text.split(' ',1)[1]
     try:
         Decrypt = secureme.decrypt(m)
         
-        await message.reply_text(Decrypt)
+        await message.reply_text(f"`{Decrypt}`")
         
 
     except Exception as e:
-        await message.reply_text(e)
+        await message.reply_text(f"{e}")
