37f3a6ca118f1b3344660c1b2125bfbe14ce114d
ɴᴏᴏʙ - ᴍᴜᴋᴇsʜ <108251605+Noob-mukesh@users.noreply.github.com>
2023-04-08 21:06:53 +0530

    Update encrypt.py

--- MukeshRobot/modules/encrypt.py
+++ MukeshRobot/modules/encrypt.py
@@ -3,8 +3,8 @@ from pyrogram import filters
 from MukeshRobot import pbot as mukesh
 
 
-@mukesh.on_message(filters.command("encypt"))
-async def countryinfo(bot, message):
+@mukesh.on_message(filters.command("encrypt"))
+async def encyrpt(bot, message):
     if len(message.command) < 2:
         return await message.reply_text("**Example:**\n\n`/encyrpt India`")
     m = message.text.split(' ',1)[1]
@@ -12,6 +12,20 @@ async def countryinfo(bot, message):
         Secure = secureme.encrypt(m)
         
         await message.reply_text(Secure)
+        
+
+    except Exception as e:
+        await message.reply_text(e)
+
+@mukesh.on_message(filters.command("decrypt"))
+async def decrypt(bot, message):
+    if len(message.command) < 2:
+        return await message.reply_text("**Example:**\n\n`/encyrpt India`")
+    m = message.text.split(' ',1)[1]
+    try:
+        Decrypt = secureme.decrypt(m)
+        
+        await message.reply_text(Decrypt)
         
 
     except Exception as e:
