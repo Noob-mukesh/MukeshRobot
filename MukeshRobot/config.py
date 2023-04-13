933b60419a5f5b26ec56bb59b2b43288c5eb0d2a
ɴᴏᴏʙ - ᴍᴜᴋᴇsʜ <108251605+Noob-mukesh@users.noreply.github.com>
2023-04-13 22:58:58 +0530

    Update config.py

--- MukeshRobot/config.py
+++ MukeshRobot/config.py
@@ -4,7 +4,7 @@ import os
 
 
 def get_user_list(config, key):
-    with open("{}/MukeshRobot/{}".format(os.getcwd(), config), "r") as json_file:
+    with open("MukeshRobot/{}".format(os.getcwd(), config), "r") as json_file:
         return json.load(json_file)[key]
 
 
