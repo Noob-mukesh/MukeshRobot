c69d77a9cad224bb26be10fa933dce1a9c4bb28b
ɴᴏᴏʙ - ᴍᴜᴋᴇsʜ <108251605+Noob-mukesh@users.noreply.github.com>
2023-04-19 13:34:20 +0530

    Update source.py

--- MukeshRobot/modules/source.py
+++ MukeshRobot/modules/source.py
@@ -9,7 +9,7 @@ from telethon import __version__ as s
 from MukeshRobot import OWNER_ID, dispatcher
 from MukeshRobot import pbot as client
 
-Mukesh = "https://telegra.ph/file/3117ae688980a7163ab5e.jpg"
+Mukesh = "https://te.legra.ph/file/1a72f3770dcb90ee8b3f7.jpg"
 
 
 @client.on_message(filters.command(["repo", "source"]))
