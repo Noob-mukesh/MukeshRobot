ac69be97ef2ce288bcbff067c3688058634a9654
ɴᴏᴏʙ - ᴍᴜᴋᴇsʜ <108251605+Noob-mukesh@users.noreply.github.com>
2023-04-25 23:42:15 +0530

    Add files via upload

---
+++ MukeshRobot/modules/truth_dare.py
@@ -0,0 +1,31 @@
+import requests
+from telegram import Update
+from telegram.ext import CallbackContext
+
+from MukeshRobot import dispatcher
+from MukeshRobot.modules.disable import DisableAbleCommandHandler
+
+
+def truth(update: Update, context: CallbackContext):
+    truth = requests.get(f"https://api.truthordarebot.xyz/v1/truth").json()["question"]
+    update.effective_message.reply_text(truth)
+
+
+def dare(update: Update, context: CallbackContext):
+    dare = requests.get(f"https://api.truthordarebot.xyz/v1/dare").json()["question"]
+    update.effective_message.reply_text(dare)
+
+
+TRUTH_HANDLER = DisableAbleCommandHandler("truth", truth)
+DARE_HANDLER = DisableAbleCommandHandler("dare", dare)
+
+dispatcher.add_handler(TRUTH_HANDLER)
+dispatcher.add_handler(DARE_HANDLER)
+
+
+__help__ = """
+*ᴛʀᴜᴛʜ & ᴅᴀʀᴇ*
+ ❍ /truth  *:* sᴇɴᴅs ᴀ ʀᴀɴᴅᴏᴍ ᴛʀᴜᴛʜ sᴛʀɪɴɢ.
+ ❍ /dare  *:* sᴇɴᴅs ᴀ ʀᴀɴᴅᴏᴍ ᴅᴀʀᴇ sᴛʀɪɴɢ.
+"""
+__mod_name__ = "⍟ ꜰᴜɴ ⍟"
