4e8278ddb64e1a76ed82e6add8f9a513984d443f
ɴᴏᴏʙ - ᴍᴜᴋᴇsʜ <108251605+Noob-mukesh@users.noreply.github.com>
2023-04-26 15:14:58 +0530

    Update dicegame.py

--- MukeshRobot/modules/dicegame.py
+++ MukeshRobot/modules/dicegame.py
@@ -10,34 +10,34 @@ from pyrogram.handlers import MessageHandler
 async def dice(bot, message):
     x=await bot.send_dice(message.chat.id)
     m=x.dice.value
-    await message.reply_text(f"Hey {message.from_user.first_name} your Score is : {m}")
+    await message.reply_text(f"Hey {message.from_user.mention} your Score is : {m}",quote=True)
   
 @mukesh.on_message(filters.command("dart"))
 async def dart(bot, message):
     x=await bot.send_dice(message.chat.id, "🎯")
     m=x.dice.value
-    await message.reply_text(f"Hey {message.from_user.first_name} your Score is : {m}")
+    await message.reply_text(f"Hey {message.from_user.mention} your Score is : {m}",quote=True)
 
 @mukesh.on_message(filters.command("basket"))
 async def basket(bot, message):
     x=await bot.send_dice(message.chat.id, "🏀")
     m=x.dice.value
-    await message.reply_text(f"Hey {message.from_user.first_name} your Score is : {m}")
+    await message.reply_text(f"Hey {message.from_user.mention} your Score is : {m}",quote=True)
 @mukesh.on_message(filters.command("jackpot"))
 async def basket(bot, message):
     x=await bot.send_dice(message.chat.id, "🎰")
     m=x.dice.value
-    await message.reply_text(f"Hey {message.from_user.first_name} your Score is : {m}")
+    await message.reply_text(f"Hey {message.from_user.mention} your Score is : {m}",quote=True)
 @mukesh.on_message(filters.command("ball"))
 async def basket(bot, message):
     x=await bot.send_dice(message.chat.id, "🎳")
     m=x.dice.value
-    await message.reply_text(f"Hey {message.from_user.first_name} your Score is : {m}")
-@mukesh.on_message(filters.command("basket"))
+    await message.reply_text(f"Hey {message.from_user.mention} your Score is : {m}",quote=True)
+@mukesh.on_message(filters.command("football"))
 async def basket(bot, message):
     x=await bot.send_dice(message.chat.id, "⚽")
     m=x.dice.value
-    await message.reply_text(f"Hey {message.from_user.first_name} your Score is : {m}")
+    await message.reply_text(f"Hey {message.from_user.mention} your Score is : {m}",quote=True)
 __help__ = """
  Play Game With Emojis:
 /dice - Dice 🎲
