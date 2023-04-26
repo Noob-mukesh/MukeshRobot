b8757cbf9d0821a5b1f5e1a042ffa8b19678e66a
ɴᴏᴏʙ - ᴍᴜᴋᴇsʜ <108251605+Noob-mukesh@users.noreply.github.com>
2023-04-26 15:46:47 +0530

    Update fonts.py

--- MukeshRobot/modules/fonts.py
+++ MukeshRobot/modules/fonts.py
@@ -95,7 +95,7 @@ async def nxt(c, m):
                 InlineKeyboardButton("S̶t̶r̶i̶k̶e̶", callback_data="style+strike"),
                 InlineKeyboardButton("F༙r༙o༙z༙e༙n༙", callback_data="style+frozen"),
             ],
-            [InlineKeyboardButton ("ᴄʟᴏsᴇ",callback_data="close_reply",InlineKeyboardButton ("ʙᴀᴄᴋ", callback_data="nxt+0")],
+            [InlineKeyboardButton ("ᴄʟᴏsᴇ",callback_data="close_reply"),InlineKeyboardButton ("ʙᴀᴄᴋ", callback_data="nxt+0")],
         ]
         await m.answer()
         await m.message.edit_reply_markup(InlineKeyboardMarkup(buttons))
