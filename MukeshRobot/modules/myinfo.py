3532b2c80e7673a3b09c0e6b0e4c9d0a10945265
ɴᴏᴏʙ - ᴍᴜᴋᴇsʜ <108251605+Noob-mukesh@users.noreply.github.com>
2023-05-02 15:48:42 +0530

    Update myinfo.py

--- MukeshRobot/modules/myinfo.py
+++ MukeshRobot/modules/myinfo.py
@@ -5,18 +5,18 @@ from datetime import datetime
 
 from telethon import custom, events
 
-from MukeshRobot import telethn as bot
+from MukeshRobot import telethn as bot,BOT_NAME
 from MukeshRobot import telethn as tgbot
 from MukeshRobot.events import register
 
 edit_time = 5
-""" =======================𝐌𝐮𝐤𝐞𝐬𝐡 𝐑𝐨𝐛𝐨𝐭====================== """
+f""" ======================={BOT_NAME}====================== """
 file1 = "https://telegra.ph/file/9a85d0a873e2dd80d278d.jpg"
 file2 = "https://telegra.ph/file/9e7815284031452afa9e5.jpg"
 file3 = "https://telegra.ph/file/dcc5e003287f69acea368.jpg"
 file4 = "https://telegra.ph/file/ed1ce7fee94f46b0f671e.jpg"
 file5 = "https://telegra.ph/file/701028ce085ecfa961a36.jpg"
-""" =======================𝐌𝐮𝐤𝐞𝐬𝐡 𝐑𝐨𝐛𝐨𝐭====================== """
+""" ======================={BOT_NAME}====================== """
 
 
 @register(pattern="/myinfo")
@@ -28,7 +28,7 @@ async def proboyx(event):
     on = await bot.send_file(
         event.chat_id,
         file=file2,
-        caption=f"Hey {firstname}, \n Click on the button below \n to get info about you",
+        caption=f"ʜᴇʏ {firstname}, \nᴄʟɪᴄᴋ ᴏɴ ʙᴜᴛᴛᴏɴ ʙᴇʟᴏᴡ \n ᴛᴏ ɢᴇᴛ ɪɴғᴏ ᴀʙᴏᴜᴛ ʏᴏᴜ",
         buttons=button,
     )
 
@@ -65,7 +65,7 @@ async def callback_query_handler(event):
     try:
         boy = event.sender_id
         PRO = await bot.get_entity(boy)
-        LILIE = "ᴘᴏᴡᴇʀᴇᴅ ʙʏ 𝐌𝐚𝐬𝐭𝐞𝐫𝐦𝐢𝐧𝐝 𝐍𝐞𝐭𝐰𝐨𝐫𝐤\n\n"
+        LILIE = f"ᴘᴏᴡᴇʀᴇᴅ ʙʏ {BOT_NAME}\n\n"
         LILIE += f"ғɪʀsᴛ ɴᴀᴍᴇ: {PRO.first_name} \n"
         LILIE += f"ʟᴀsᴛ ɴᴀᴍᴇ: {PRO.last_name}\n"
         LILIE += f"ʏᴏᴜ ʙᴏᴛ : {PRO.bot} \n"
