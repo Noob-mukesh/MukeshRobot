15f8d01129fd0ca9667e3cc6cc1082f324c069ad
ɴᴏᴏʙ - ᴍᴜᴋᴇsʜ <108251605+Noob-mukesh@users.noreply.github.com>
2023-05-02 15:38:32 +0530

    Create figlet.py

---
+++ MukeshRobot/modules/figlet.py
@@ -0,0 +1,35 @@
+from pyrogram import filters
+import asyncio
+import pyfiglet 
+from random import choice
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message, CallbackQuery
+from pyrogram.handlers import MessageHandler
+from .. import bot as Client
+def figle(text):
+    x = pyfiglet.FigletFont.getFonts()
+    font = choice(x)
+    figled = str(pyfiglet.figlet_format(text,font=font))
+    keyboard = InlineKeyboardMarkup([[InlineKeyboardButton(text="ᴄʜᴀɴɢᴇ", callback_data="change_quote"),InlineKeyboardButton(text="ᴄʟᴏsᴇ", callback_data="mainclose")]])
+    return figled, keyboard
+
+@Client.on_message(filters.command("figlet"))
+async def echo(bot, message):
+    global text
+    try:
+        text = message.text.split(' ',1)[1]
+    except IndexError:
+        return await message.reply_text("Example:\n\n/figlet Mukesh")
+    kul_text, keyboard = figle(text)
+    await message.reply_text(f"<pre>{kul_text}</pre>", quote=True, reply_markup=keyboard)
+
+@Client.on_callback_query(filters.regex("^change_quote$"))
+async def figlet_handler(Client, query: CallbackQuery):
+  try:
+      kul_text, keyboard = figle(text)
+      await query.message.edit_text(f"<pre>{kul_text}</pre>", reply_markup=keyboard)
+  except Exception as e : 
+      await message.reply(e)
+__mod_name__ = "⍟ Fɪɢʟᴇᴛ ⍟" 
+__help__="""
+❍ /figlet*:* ᴍᴀᴋᴇs ғɪɢʟᴇᴛ ᴏғ ᴛʜᴇ ɢɪᴠᴇɴ ᴛᴇxᴛ
+Example:\n\n/figlet Mukesh"""
