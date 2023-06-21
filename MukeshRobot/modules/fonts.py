95760c4b22d500920a84df03cdc984e8ef2b456d
ɴᴏᴏʙ - ᴍᴜᴋᴇsʜ <108251605+Noob-mukesh@users.noreply.github.com>
2023-06-21 09:18:22 +0530

    Update fonts.py

--- MukeshRobot/modules/fonts.py
+++ MukeshRobot/modules/fonts.py
@@ -187,7 +187,7 @@ async def style(c, m):
     if style == "frozen":
         cls = Fonts.frozen
     #text = m.text.split(' ',1)[1]    
-    new_text = cls(m.message.reply_to_message.text.split(" ",1)[1]
+    new_text = cls(m.message.reply_to_message.text.split(" ",1)[1])
     try:
         await m.message.edit_text(new_text, reply_markup=m.message.reply_markup)
     except:
