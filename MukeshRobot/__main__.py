014ffc507b390ddbceb1cbdf5356ce615da681e4
ɴᴏᴏʙ - ᴍᴜᴋᴇsʜ <108251605+Noob-mukesh@users.noreply.github.com>
2023-06-26 22:01:24 +0530

    Update __main__.py

--- MukeshRobot/__main__.py
+++ MukeshRobot/__main__.py
@@ -935,7 +935,8 @@ def main():
     Music_callback_handler = CallbackQueryHandler(
         Music_about_callback, pattern=r"Music_"
     )
-
+    MukeshRobot_Main_handler = CallbackQueryHandler(
+        MukeshRobot_Main_Callback, pattern=r"Main_help")
     donate_handler = CommandHandler("donate", donate)
     migrate_handler = MessageHandler(Filters.status_update.migrate, migrate_chats)
     dispatcher.add_handler(start_handler)
@@ -947,7 +948,7 @@ def main():
     dispatcher.add_handler(settings_callback_handler)
     dispatcher.add_handler(migrate_handler)
     dispatcher.add_handler(donate_handler)
-    dispatcher.add_handler(MukeshRobot_Main_Callback)
+    dispatcher.add_handler(MukeshRobot_Main_handler)
     dispatcher.add_error_handler(error_callback)
 
     LOGGER.info("Using long polling.")
