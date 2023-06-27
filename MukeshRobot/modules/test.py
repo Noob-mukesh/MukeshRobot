2bf8a2005cff9c12a4f7df82a9c17212e789a0a1
ɴᴏᴏʙ - ᴍᴜᴋᴇsʜ <108251605+Noob-mukesh@users.noreply.github.com>
2023-06-28 02:24:05 +0530

    Update test.py

--- MukeshRobot/modules/test.py
+++ MukeshRobot/modules/test.py
@@ -10,9 +10,6 @@ from MukeshRobot import DEV_USERS, LOGGER, OWNER_ID, dispatcher
 from MukeshRobot.modules.helper_funcs.chat_status import dev_plus, sudo_plus
 from MukeshRobot.modules.no_sql.users_db import get_all_users
 
-USERS_GROUP = 4
-CHAT_GROUP = 5
-DEV_AND_MORE = DEV_USERS.append(int(OWNER_ID))
 
 
 def get_user_id(username):
@@ -43,16 +40,10 @@ def get_user_id(username):
 
     return None
 
-AUTO_SLEEP = 5
-IS_BROADCASTING = False
-cleanmode_group = 15
-
-
-
 @app.on_message(filters.command(" broadcast") & filters.user(OWNER_ID))
 
 async def braodcast_message(client, message, _):
-    global IS_BROADCASTING
+    
     if message.reply_to_message:
         x = message.reply_to_message.message_id
         y = message.chat.id
@@ -71,74 +62,28 @@ async def braodcast_message(client, message, _):
         if query == "":
             return await message.reply_text("ᴘʟᴇᴀsᴇ ᴘʀᴏᴠɪᴅᴇ sᴏᴍᴇ ᴛᴇxᴛ ᴛᴏ ʙʀᴏᴀᴅᴄᴀsᴛ")
 
-    IS_BROADCASTING = True
+    
 
     # Bot broadcast inside chats
     if "-nobot" not in message.text:
         sent = 0
         pin = 0
         chats = []
-        schats = await user_db.get_all_chats() or []
+        schats = await user_db.get_all_chats() 
         for chat in schats:
             chats.append(int(chat["chat_id"]))
         for i in chats:
-            if i == -1001686672798:
-                continue
+            
             try:
                 m = (
                     await app.forward_messages(i, y, x)
                     if message.reply_to_message
                     else await app.send_message(i, text=query)
                 )
-                if "-pin" in message.text:
-                    try:
-                        await m.pin(disable_notification=True)
-                        pin += 1
-                    except Exception:
-                        continue
-                elif "-pinloud" in message.text:
-                    try:
-                        await m.pin(disable_notification=False)
-                        pin += 1
-                    except Exception:
-                        continue
-                sent += 1
-            except FloodWait as e:
-                flood_time = int(e.x)
-                if flood_time > 200:
-                    continue
-                await asyncio.sleep(flood_time)
-            except Exception:
-                continue
-        try:
-            await message.reply_text(_["broad_1"].format(sent, pin))
-        except:
-            pass
-
-    # Bot broadcasting to users
-    if "-user" in message.text:
-        susr = 0
-        served_users = []
-        susers = await get_all_users()
-        for user in susers:
-            served_users.append(int(user["_id"]))
-        for i in served_users:
-            try:
-                m = (
-                    await app.forward_messages(i, y, x)
-                    if message.reply_to_message
-                    else await app.send_message(i, text=query)
-                )
-                susr += 1
-            except FloodWait as e:
-                flood_time = int(e.x)
-                if flood_time > 200:
-                    continue
-                await asyncio.sleep(flood_time)
-            except Exception:
-                pass
+               
         try:
             await message.reply_text("ʙʀᴏᴀᴅᴄᴀsᴛᴇᴅ ᴍᴇssᴀɢᴇ ᴛᴏ {0} ᴜsᴇʀs".format(susr))
-        except:
-            pass
-    IS_BROADCASTING = False    
+        except Exception as e:
+            await message.reply_text(e)
+            
+     
