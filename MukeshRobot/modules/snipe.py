caf6b10a413dce0c625126a7ffe777729aed7f5d
ᴅᴇᴠᴀɴsʜ-ʙᴏᴛs <125127646+Devansh-Bots@users.noreply.github.com>
2023-06-27 22:21:05 +0530

    Update snipe.py

--- MukeshRobot/modules/snipe.py
+++ MukeshRobot/modules/snipe.py
@@ -32,7 +32,7 @@ __help__ = """
 ᴍᴀᴋᴇ ᴍᴇ sᴇɴᴅ ᴀ ᴍᴇssᴀɢᴇ ᴛᴏ ᴀ sᴘᴇᴄɪғɪᴄ ᴄʜᴀᴛ.
 """
 
-__mod_name__ = "⍟ sɴɪᴘᴇ ⍟"
+__mod_name__ = "Sɴɪᴘᴇ"
 
 SNIPE_HANDLER = CommandHandler(
     "snipe", snipe, pass_args=True, filters=CustomFilters.dev_filter
