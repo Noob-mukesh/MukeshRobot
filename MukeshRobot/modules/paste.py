f14cd4e781a7c29da154b33501bf1d37bbd09674
ᴅᴇᴠᴀɴsʜ-ʙᴏᴛs <125127646+Devansh-Bots@users.noreply.github.com>
2023-06-27 22:16:23 +0530

    Update paste.py

--- MukeshRobot/modules/paste.py
+++ MukeshRobot/modules/paste.py
@@ -62,7 +62,7 @@ async def paste_func(_, message):
     return await m.edit(link)
 
 
-__mod_name__ = "⍟ Pᴀs​ᴛᴇ ⍟"
+__mod_name__ = "Pᴀs​ᴛᴇ"
 __help__ = """
  ᴘᴀsᴛᴇs ᴛʜᴇ ɢɪᴠᴇɴ ғɪʟᴇ ᴀɴᴅ sʜᴏᴡs ʏᴏᴜ ᴛʜᴇ ʀᴇsᴜʟᴛ
 
