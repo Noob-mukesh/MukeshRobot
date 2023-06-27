0fffdc1209e3d659f4062d0bea1543b30856d5d4
ᴅᴇᴠᴀɴsʜ-ʙᴏᴛs <125127646+Devansh-Bots@users.noreply.github.com>
2023-06-27 22:22:46 +0530

    Update speed_test.py

--- MukeshRobot/modules/speed_test.py
+++ MukeshRobot/modules/speed_test.py
@@ -64,7 +64,7 @@ __help__ = """
 » /speedtest *:* ʀᴜɴs ᴀ sᴘᴇᴇᴅᴛᴇsᴛ ᴀɴᴅ ᴄʜᴇᴄᴋ ᴛʜᴇ sᴇʀᴠᴇʀ sᴘᴇᴇᴅ.
 """
 
-__mod_name__ = "⍟ SᴘᴇᴇᴅTᴇsᴛ ⍟​"
+__mod_name__ = "SᴘᴇᴇᴅTᴇsᴛ​"
 
 __command_list__ = ["speedtest"]
 __handlers__ = [SPEED_TEST_HANDLER, SPEED_TEST_CALLBACKHANDLER]
