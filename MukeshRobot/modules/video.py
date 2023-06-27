134090c8f1300aeaa2b145e87b89ccaf33f13cc4
ᴅᴇᴠᴀɴsʜ-ʙᴏᴛs <125127646+Devansh-Bots@users.noreply.github.com>
2023-06-27 22:28:33 +0530

    Update video.py

--- MukeshRobot/modules/video.py
+++ MukeshRobot/modules/video.py
@@ -105,7 +105,7 @@ async def ytmusic(client, message: Message):
             os.remove(files)
 
 
-__mod_name__ = "⍟ ᴠɪᴅᴇᴏ ⍟"
+__mod_name__ = "Vɪᴅᴇᴏ"
 __help_ = """ 
 /video to download video song
 /vsong to download video song """
