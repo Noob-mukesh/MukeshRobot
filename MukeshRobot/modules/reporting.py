c9041f0a08087df789722427d46b232fd920ab1b
ᴅᴇᴠᴀɴsʜ-ʙᴏᴛs <125127646+Devansh-Bots@users.noreply.github.com>
2023-06-27 22:18:32 +0530

    Update reporting.py

--- MukeshRobot/modules/reporting.py
+++ MukeshRobot/modules/reporting.py
@@ -289,7 +289,7 @@ dispatcher.add_handler(SETTING_HANDLER)
 dispatcher.add_handler(REPORT_HANDLER, REPORT_GROUP)
 dispatcher.add_handler(ADMIN_REPORT_HANDLER, REPORT_GROUP)
 
-__mod_name__ = "⍟ ʀᴇᴘᴏʀᴛs​ ⍟"
+__mod_name__ = "Rᴇᴘᴏʀᴛs​"
 __handlers__ = [
     (REPORT_HANDLER, REPORT_GROUP),
     (ADMIN_REPORT_HANDLER, REPORT_GROUP),
