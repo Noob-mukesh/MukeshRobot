31c04b1986f499476bf6c80ce349ec124cdfe27b
ɴᴏᴏʙ - ᴍᴜᴋᴇsʜ <108251605+Noob-mukesh@users.noreply.github.com>
2023-06-28 09:14:18 +0530

    Update truth_dare.py

--- MukeshRobot/modules/truth_dare.py
+++ MukeshRobot/modules/truth_dare.py
@@ -14,28 +14,28 @@ def truth(update: Update, context: CallbackContext):
 def dare(update: Update, context: CallbackContext):
     dare = requests.get(f"https://api.truthordarebot.xyz/v1/dare").json()["question"]
     update.effective_message.reply_text(dare)
-def wyr(update: Update, context: CallbackContext):
-    truth = requests.get(f"https://api.truthordarebot.xyz/v1/wyr").json()["question"]
-    update.effective_message.reply_text(wyr)
+#def wyr(update: Update, context: CallbackContext):
+#    truth = requests.get(f"https://api.truthordarebot.xyz/v1/wyr").json()["question"]
+#    update.effective_message.reply_text(wyr)
 
-def nhie(update: Update, context: CallbackContext):
-    truth = requests.get(f"https://api.truthordarebot.xyz/v1/nhie").json()["question"]
-    update.effective_message.reply_text(nhie)
-def paranoia(update: Update, context: CallbackContext):
-    truth = requests.get(f"https://api.truthordarebot.xyz/v1/paranoia").json()["question"]
-    update.effective_message.reply_text(paranoia)
+#def nhie(update: Update, context: CallbackContext):
+#    truth = requests.get(f"https://api.truthordarebot.xyz/v1/nhie").json()["question"]
+#    update.effective_message.reply_text(nhie)
+#def paranoia(update: Update, context: CallbackContext):
+#    truth = requests.get(f"https://api.truthordarebot.xyz/v1/paranoia").json()["question"]
+#    update.effective_message.reply_text(paranoia)
 
 TRUTH_HANDLER = DisableAbleCommandHandler("truth", truth, run_async=True)
 DARE_HANDLER = DisableAbleCommandHandler("dare", dare, run_async=True)
-WYR_HANDLER = DisableAbleCommandHandler("wyr", wyr, run_async=True)
-NHIE_HANDLER = DisableAbleCommandHandler("nhie", nhie, run_async=True)
-PARANIOA_HANDLER = DisableAbleCommandHandler("paranoia", paranoia, run_async=True)
+#WYR_HANDLER = DisableAbleCommandHandler("wyr", wyr, run_async=True)
+#NHIE_HANDLER = DisableAbleCommandHandler("nhie", nhie, run_async=True)
+#PARANIOA_HANDLER = DisableAbleCommandHandler("paranoia", paranoia, run_async=True)
 dispatcher.add_handler(TRUTH_HANDLER)
 dispatcher.add_handler(DARE_HANDLER)
-dispatcher.add_handler(WYR_HANDLER)
+#dispatcher.add_handler(WYR_HANDLER)
 
-dispatcher.add_handler(NHIE_HANDLER)
-dispatcher.add_handler(PARANIOA_HANDLER)
+#dispatcher.add_handler(NHIE_HANDLER)
+#dispatcher.add_handler(PARANIOA_HANDLER)
 
 
 __help__ = """
@@ -43,4 +43,4 @@ __help__ = """
  ❍ /truth  *:* sᴇɴᴅs ᴀ ʀᴀɴᴅᴏᴍ ᴛʀᴜᴛʜ sᴛʀɪɴɢ.
  ❍ /dare  *:* sᴇɴᴅs ᴀ ʀᴀɴᴅᴏᴍ ᴅᴀʀᴇ sᴛʀɪɴɢ.
 """
-__mod_name__ = "T - D"
+__mod_name__ = "ᴛ-ᴅ"
