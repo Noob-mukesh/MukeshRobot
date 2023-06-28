4f0b21c71d4d33b527a5f1cc4d15f9466661864e
ɴᴏᴏʙ - ᴍᴜᴋᴇsʜ <108251605+Noob-mukesh@users.noreply.github.com>
2023-06-28 23:47:06 +0530

    Update afk.py

--- MukeshRobot/modules/afk.py
+++ MukeshRobot/modules/afk.py
@@ -3,9 +3,31 @@ from pyrogram import filters
 from pyrogram.types import Message
 
 from MukeshRobot import pbot
-from MukeshRobot.modules.helper_funcs.readable_time import get_readable_time
-from MukeshRobot.modules.no_sql.afk_db import add_afk, is_afk, remove_afk
 
+from MukeshRobot.modules.no_sql.afk_db import add_afk, is_afk, remove_afk
+def get_readable_time(seconds: int) -> str:
+    count = 0
+    readable_time = ""
+    time_list = []
+    time_suffix_list = ["s", "ᴍ", "ʜ", "ᴅᴀʏs"]
+
+    while count < 4:
+        count += 1
+        remainder, result = divmod(seconds, 60) if count < 3 else divmod(seconds, 24)
+        if seconds == 0 and remainder == 0:
+            break
+        time_list.append(int(result))
+        seconds = int(remainder)
+
+    for x in range(len(time_list)):
+        time_list[x] = str(time_list[x]) + time_suffix_list[x]
+    if len(time_list) == 4:
+        readable_time += time_list.pop() + ", "
+
+    time_list.reverse()
+    readable_time += ":".join(time_list)
+
+    return readable_time
 
 @pbot.on_message(filters.command(["afk", "brb","bye"]))
 async def active_afk(_, message: Message):
