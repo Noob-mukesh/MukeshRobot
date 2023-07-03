ec574ee678b4e855be7d19eb084c5cfda1133a6d
ɴᴏᴏʙ - ᴍᴜᴋᴇsʜ <108251605+Noob-mukesh@users.noreply.github.com>
2023-07-03 11:20:22 +0530

    Update cust_filters.py

--- MukeshRobot/modules/cust_filters.py
+++ MukeshRobot/modules/cust_filters.py
@@ -581,7 +581,7 @@ def addnew_filter(update, chat_id, keyword, text, file_type, file_id, buttons):
 
 
 def __stats__():
-    return "• {} filters, across {} chats.".format(sql.num_filters(), sql.num_chats())
+    return "• {} ғɪʟᴛᴇʀs, ᴀᴄʀᴏss {} ᴄʜᴀᴛs.".format(sql.num_filters(), sql.num_chats())
 
 
 def __import_data__(chat_id, data):
