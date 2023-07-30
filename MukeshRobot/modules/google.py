6916dc93a85f230058294b5a06370e4a2f942e52
ɴᴏᴏʙ - ᴍᴜᴋᴇsʜ <108251605+Noob-mukesh@users.noreply.github.com>
2023-07-30 07:38:17 +0530

    Update google.py

--- MukeshRobot/modules/google.py
+++ MukeshRobot/modules/google.py
@@ -258,7 +258,7 @@ async def apk(e):
             + app_link
             + "'>View in Play Store</a>"
         )
-        app_details += "\n\n===> Fallen <==="
+        app_details += "\n\n===> Group Controller<==="
         await e.reply(app_details, link_preview=True, parse_mode="HTML")
     except IndexError:
         await e.reply("No result found in search. Please enter **Valid app name**")
