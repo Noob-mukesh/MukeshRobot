c6c96a28217c6136901f699537d2ab36f02e630e
ɴᴏᴏʙ - ᴍᴜᴋᴇsʜ <108251605+Noob-mukesh@users.noreply.github.com>
2023-08-20 19:50:53 +0530

    fixed type error

--- MukeshRobot/modules/cust_filters.py
+++ MukeshRobot/modules/cust_filters.py
@@ -407,8 +407,7 @@ def reply_filter(update, context):
                             caption=markdown_to_html(filtext),
                             reply_to_message_id=message.message_id,
                             parse_mode=ParseMode.HTML,
-                            disable_web_page_preview=True,
-                            reply_markup=keyboard,
+                            reply_markup=keyboard
                         )
                     except BadRequest:
                         send_message(
