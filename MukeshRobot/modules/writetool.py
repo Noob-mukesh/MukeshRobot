4862ce45c4444c8a475c45c6cf35aba99b341f98
ɴᴏᴏʙ - ᴍᴜᴋᴇsʜ <108251605+Noob-mukesh@users.noreply.github.com>
2023-08-20 18:36:02 +0530

    fixed writetool api

--- MukeshRobot/modules/writetool.py
+++ MukeshRobot/modules/writetool.py
@@ -1,60 +1,48 @@
-import requests
+"""MIT License
+
+Copyright (c) 2023-24 Noob-Mukesh
+
+          GITHUB: NOOB-MUKESH
+          TELEGRAM: @MR_SUKKUN
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE."""
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
-
 from MukeshRobot import BOT_NAME, BOT_USERNAME
 from MukeshRobot import pbot as mukesh
-
-
+import requests
 @mukesh.on_message(filters.command("write"))
 async def handwrite(_, message: Message):
-    if not message.reply_to_message:
-        text = message.text.split(None, 1)[1]
-        m = await mukesh.send_message(
-            message.chat.id, "`Please wait...,\n\nWriting your text...`"
-        )
-        API = f"https://api.sdbots.tk/write?text={text}"
-        req = requests.get(API).url
-        caption = f"""
-sᴜᴄᴇssғᴜʟʟʏ ᴡʀɪᴛᴛᴇɴ ᴛᴇxᴛ 💘
-
-✨ **ᴡʀɪᴛᴛᴇɴ ʙʏ :** [{BOT_NAME}](https://t.me/{BOT_USERNAME})
-🥀 **ʀᴇǫᴜᴇsᴛᴇᴅ ʙʏ :** {message.from_user.mention}
-❄ **ʟɪɴᴋ :** `{req}`
-"""
-        await m.delete()
-        await mukesh.send_photo(
-            message.chat.id,
-            photo=req,
-            caption=caption,
-            reply_markup=InlineKeyboardMarkup(
-                [[InlineKeyboardButton("• ᴛᴇʟᴇɢʀᴀᴩʜ •", url=f"{req}")]]
-            ),
-        )
+    if message.reply_to_message:
+        text = message.reply_to_message.text
     else:
-        lol = message.reply_to_message.text
-        m = await mukesh.send_message(
-            message.chat.id, "`Please wait...,\n\nWriting your text...`"
-        )
-        API = f"https://api.sdbots.tk/write?text={lol}"
-        req = requests.get(API).url
-        caption = f"""
-sᴜᴄᴇssғᴜʟʟʏ ᴡʀɪᴛᴛᴇɴ ᴛᴇxᴛ 💘
+        text =message.text.split(None, 1)[1]
+    m =await message.reply_text( "`Please wait...,\n\nWriting your text...`")
+    write = requests.get(f"https://apis.xditya.me/write?text={text}").url
 
+    caption = f"""
+sᴜᴄᴇssғᴜʟʟʏ ᴡʀɪᴛᴛᴇɴ ᴛᴇxᴛ 💘
 ✨ **ᴡʀɪᴛᴛᴇɴ ʙʏ :** [{BOT_NAME}](https://t.me/{BOT_USERNAME})
 🥀 **ʀᴇǫᴜᴇsᴛᴇᴅ ʙʏ :** {message.from_user.mention}
-❄ **ʟɪɴᴋ :** `{req}`
 """
-        await m.delete()
-        await mukesh.send_photo(
-            message.chat.id,
-            photo=req,
-            caption=caption,
-            reply_markup=InlineKeyboardMarkup(
-                [[InlineKeyboardButton("• ᴛᴇʟᴇɢʀᴀᴩʜ •", url=f"{req}")]]
-            ),
-        )
-
+    await m.delete()
+    await message.reply_photo(photo=write,caption=caption)
 
 __mod_name__ = "WʀɪᴛᴇTᴏᴏʟ"
 
