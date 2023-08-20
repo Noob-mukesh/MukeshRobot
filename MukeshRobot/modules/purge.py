02d06c2a066d9679e676beaa9a57ce23d27bb8b6
ɴᴏᴏʙ - ᴍᴜᴋᴇsʜ <108251605+Noob-mukesh@users.noreply.github.com>
2023-08-20 18:38:31 +0530

    added spurge

--- MukeshRobot/modules/purge.py
+++ MukeshRobot/modules/purge.py
@@ -2,7 +2,7 @@ import time
 
 from telethon import events
 
-from MukeshRobot import telethn
+from MukeshRobot import telethn,pbot
 from MukeshRobot.modules.helper_funcs.telethn.chatstatus import (
     can_delete_messages,
     user_is_admin,
@@ -44,7 +44,7 @@ async def purge_messages(event):
     except:
         pass
     time_ = time.perf_counter() - start
-    text = f"Purged Successfully in {time_:0.2f} Second(s)\nFast AF 😎"
+    text = f"ᴘᴜʀɢᴇᴅ ꜱᴜᴄᴄᴇꜱꜱꜰᴜʟʟʏ ɪɴ {time_:0.2f} ꜱᴇᴄᴏɴᴅ(s)\nꜰᴀꜱᴛ ᴀꜰ 😎"
     await event.respond(text, parse_mode="markdown")
 
 
@@ -69,20 +69,53 @@ async def delete_messages(event):
     chat = await event.get_input_chat()
     del_message = [message, event.message]
     await event.client.delete_messages(chat, del_message)
+async def spurge_messages(event):
+    if event.from_id is None:
+        return
+    if not await user_is_admin(
+        user_id=event.sender_id, message=event
+    ) and event.from_id not in [1087968824]:
+        await event.reply("Only Admins are allowed to use this command")
+        return
 
+    if not await can_delete_messages(message=event):
+        await event.reply("Can't seem to purge the message")
+        return
+
+    reply_msg = await event.get_reply_message()
+    if not reply_msg:
+        await event.reply("Reply to a message to select where to start purging from.")
+        return
+    messages = []
+    message_id = reply_msg.id
+    delete_to = event.message.id
+
+    messages.append(event.reply_to_msg_id)
+    for msg_id in range(message_id, delete_to + 1):
+        messages.append(msg_id)
+        if len(messages) == 100:
+            await event.client.delete_messages(event.chat_id, messages)
+            messages = []
+
+    try:
+        await event.client.delete_messages(event.chat_id, messages)
+    except:
+        pass
 
 __help__ = """
  ❍ /del *:* ᴅᴇʟᴇᴛᴇs ᴛʜᴇ ᴍᴇssᴀɢᴇ ʏᴏᴜ ʀᴇᴘʟɪᴇᴅ ᴛᴏ
  ❍ /purge *:* ᴅᴇʟᴇᴛᴇs ᴀʟʟ ᴍᴇssᴀɢᴇs ʙᴇᴛᴡᴇᴇɴ ᴛʜɪs ᴀɴᴅ ᴛʜᴇ ʀᴇᴘʟɪᴇᴅ ᴛᴏ ᴍᴇssᴀɢᴇ.
  ❍ /purge  <ɪɴᴛᴇɢᴇʀ x>*:* ᴅᴇʟᴇᴛᴇs ᴛʜᴇ ʀᴇᴘʟɪᴇᴅ ᴍᴇssᴀɢᴇ, ᴀɴᴅ x ᴍᴇssᴀɢᴇs ғᴏʟʟᴏᴡɪɴɢ ɪᴛ ɪғ ʀᴇᴘʟɪᴇᴅ ᴛᴏ ᴀ ᴍᴇssᴀɢᴇ.
+ ❍ /spurge *:* ᴅᴇʟᴇᴛᴇs ᴀʟʟ ᴍᴇssᴀɢᴇs ʙᴇᴛᴡᴇᴇɴ ᴛʜɪs ᴀɴᴅ ᴛʜᴇ ʀᴇᴘʟɪᴇᴅ ᴛᴏ ᴍᴇssᴀɢᴇ.
 """
 
 PURGE_HANDLER = purge_messages, events.NewMessage(pattern="^[!/]purge$")
-DEL_HANDLER = delete_messages, events.NewMessage(pattern="^[!/]del$")
 
+DEL_HANDLER = delete_messages, events.NewMessage(pattern="^[!/]del$")
+SPURGE_HANDLER = spurge_messages, events.NewMessage(pattern="^[!/]spurge$")
 telethn.add_event_handler(*PURGE_HANDLER)
 telethn.add_event_handler(*DEL_HANDLER)
-
+telethn.add_event_handler(*SPURGE_HANDLER)
 __mod_name__ = "Pᴜʀɢᴇ"
-__command_list__ = ["del", "purge"]
-__handlers__ = [PURGE_HANDLER, DEL_HANDLER]
+__command_list__ = ["del", "purge","spurge"]
+__handlers__ = [PURGE_HANDLER, DEL_HANDLER,SPURGE_HANDLER]
