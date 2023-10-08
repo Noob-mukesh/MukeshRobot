6cd08f5847cdf3deb21d9991db0a34587e3d2abc
ɴᴏᴏʙ - ᴍᴜᴋᴇsʜ <108251605+Noob-mukesh@users.noreply.github.com>
2023-10-08 18:38:45 +0530

    Update misc.py

--- MukeshRobot/modules/helper_funcs/misc.py
+++ MukeshRobot/modules/helper_funcs/misc.py
@@ -80,7 +80,7 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     if calc in [1, 2]:
         pairs.append((modules[-1],))
 
-    max_num_pages = ceil(len(pairs) / 10)
+    max_num_pages = ceil(len(pairs) / 4)
     modulo_page = page_n % max_num_pages
 
     # can only have a certain amount of buttons side by side
