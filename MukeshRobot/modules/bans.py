bec51b7c5efda5277bb9e4c80dc58118243eee9c
ɴᴏᴏʙ - ᴍᴜᴋᴇsʜ <108251605+Noob-mukesh@users.noreply.github.com>
2023-10-08 22:07:10 +0530

    Update bans.py

--- MukeshRobot/modules/bans.py
+++ MukeshRobot/modules/bans.py
@@ -421,7 +421,7 @@ dispatcher.add_handler(UNBAN_HANDLER)
 dispatcher.add_handler(ROAR_HANDLER)
 dispatcher.add_handler(KICKME_HANDLER)
 
-__mod_name__ = "ʙᴀɴ"
+__mod_name__ = "Bᴀɴ"
 __handlers__ = [
     BAN_HANDLER,
     TEMPBAN_HANDLER,
