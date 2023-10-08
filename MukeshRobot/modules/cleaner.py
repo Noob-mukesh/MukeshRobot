b15f35493a54de6e133f44269d498cc8421fd393
ɴᴏᴏʙ - ᴍᴜᴋᴇsʜ <108251605+Noob-mukesh@users.noreply.github.com>
2023-10-08 22:10:14 +0530

    Update cleaner.py

--- MukeshRobot/modules/cleaner.py
+++ MukeshRobot/modules/cleaner.py
@@ -248,7 +248,7 @@ dispatcher.add_handler(REMOVE_CLEAN_BLUE_TEXT_GLOBAL_HANDLER)
 dispatcher.add_handler(LIST_CLEAN_BLUE_TEXT_HANDLER)
 dispatcher.add_handler(CLEAN_BLUE_TEXT_HANDLER, BLUE_TEXT_CLEAN_GROUP)
 
-__mod_name__ = "ᴄʟᴇᴀɴ"
+__mod_name__ = "Cʟᴇᴀɴ"
 __handlers__ = [
     SET_CLEAN_BLUE_TEXT_HANDLER,
     ADD_CLEAN_BLUE_TEXT_HANDLER,
