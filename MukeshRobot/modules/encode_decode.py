52b7311b6c0339dad02a31e8ceb41f18ca826667
ɴᴏᴏʙ - ᴍᴜᴋᴇsʜ <108251605+Noob-mukesh@users.noreply.github.com>
2024-02-06 17:12:07 +0530

    Update encode_decode.py

--- MukeshRobot/modules/encode_decode.py
+++ MukeshRobot/modules/encode_decode.py
@@ -10,11 +10,11 @@ async def passwordgen(bot, message):
         
         await bot.send_chat_action(message.chat.id, ChatAction.TYPING)
         if len(message.command) < 2:
-            await message.reply_text(
+            return await message.reply_text(
             "Example:**\n\n`/password <length>`")
         else:
             a = message.text.split(' ', 1)[1]
-            response = requests.get(f'https://mukesh-api.vercel.app/password/{a}') 
+            response = requests.get(f'https://mukesh-api.vercel.app/password?num={a}') 
             x=response.json()["results"]
             
             await message.reply_text(f"Here is your Password:- ` {x}`", parse_mode=ParseMode.MARKDOWN)     
@@ -27,11 +27,11 @@ async def morse_en(bot, message):
         
         await bot.send_chat_action(message.chat.id, ChatAction.TYPING)
         if len(message.command) < 2:
-            await message.reply_text(
+            return await message.reply_text(
             "Example:**\n\n`/morseencode <query>`")
         else:
             a = message.text.split(' ', 1)[1]
-            response = requests.get(f'https://mukesh-api.vercel.app/morse/encode/{a}') 
+            response = requests.get(f'https://mukesh-api.vercel.app/morse/encode?query={a}') 
             x=response.json()["results"]
             
             await message.reply_text(f"`{x}`", parse_mode=ParseMode.MARKDOWN)     
@@ -44,11 +44,11 @@ async def morse_de(bot, message):
         
         await bot.send_chat_action(message.chat.id, ChatAction.TYPING)
         if len(message.command) < 2:
-            await message.reply_text(
+            return await message.reply_text(
             "Example:**\n\n`/morsedecode <query>`")
         else:
             a = message.text.split(' ', 1)[1]
-            response = requests.get(f'https://mukesh-api.vercel.app/morse/decode/{a}') 
+            response = requests.get(f'https://mukesh-api.vercel.app/morse/decode?query={a}') 
             x=response.json()["results"]
             
             await message.reply_text(f"`{x}`", parse_mode=ParseMode.MARKDOWN)     
@@ -61,11 +61,11 @@ async def base_en(bot, message):
         
         await bot.send_chat_action(message.chat.id, ChatAction.TYPING)
         if len(message.command) < 2:
-            await message.reply_text(
+            return await message.reply_text(
             "Example:**\n\n`/encode <query>`")
         else:
             a = message.text.split(' ', 1)[1]
-            response = requests.get(f'https://mukesh-api.vercel.app/base/encode/{a}') 
+            response = requests.get(f'https://mukesh-api.vercel.app/base/encode?query={a}') 
             x=response.json()["results"]
             
             await message.reply_text(f"` {x}`", parse_mode=ParseMode.MARKDOWN)     
@@ -78,11 +78,11 @@ async def base_de(bot, message):
         
         await bot.send_chat_action(message.chat.id, ChatAction.TYPING)
         if len(message.command) < 2:
-            await message.reply_text(
+            return await message.reply_text(
             "Example:**\n\n`/decode <query>`")
         else:
             a = message.text.split(' ', 1)[1]
-            response = requests.get(f'https://mukesh-api.vercel.app/base/decode/{a}') 
+            response = requests.get(f'https://mukesh-api.vercel.app/base/decode?query={a}') 
             x=response.json()["results"]
             
             await message.reply_text(f" `{x}`", parse_mode=ParseMode.MARKDOWN)     
