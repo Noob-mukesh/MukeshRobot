b6317a0c83c7829edacba23d4c5fc30a6f65cf87
Razer <therazerhub@gmail.com>
2024-11-09 18:19:08 +0530

    Update config.py

--- MukeshRobot/config.py
+++ MukeshRobot/config.py
@@ -1,14 +1,14 @@
 class Config(object):
     LOGGER = True
-    API_ID =None 
-    API_HASH = ""
-    TOKEN = ""  
-    OWNER_ID=None
+    API_ID = 24139942
+    API_HASH = "89946104c3380aeb322e96bd819d77ea"
+    TOKEN = "7705269037:AAFHvns7j6j-kNOuxKI8nZ4Oo_CcI4F2ItA"  
+    OWNER_ID=7058301407
     
     SUPPORT_CHAT = "" 
-    START_IMG = ""
+    START_IMG = "https://files.catbox.moe/0wm3s8.jpg"
     EVENT_LOGS = ()
-    MONGO_DB_URI= ""
+    MONGO_DB_URI= "mongodb+srv://razer:razer@cluster1.4uj5sms.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1"
    
     DATABASE_URL = ""  # A sql database url from elephantsql.com
     CASH_API_KEY = (
